9dec6758424a90c8f5f13fc61e2a4ff3d352f6e6
Alessandro Dionisi <cecchisandrone@gmail.com>
2018-06-02 13:27:14 +0000

    Improved control

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from flask_socketio import SocketIO, emit
 import time
 import RPi.GPIO as GPIO
 from math import *
+import signal
+
+def handle_exit_signal(signal_number, stack):
+    print("Detected signal " + str(signal_number))
+    print('Quitting porterbot-controller')
+    leftMotor.stop()
+    rightMotor.stop()
+    GPIO.cleanup()
+    print('Cleanup terminated')
+    exit()
+    
+signal.signal(signal.SIGTERM, handle_exit_signal)
+signal.signal(signal.SIGINT, handle_exit_signal)
+signal.signal(signal.SIGHUP, handle_exit_signal)
 
 leftForwardPin = 22
 leftBackwardPin = 27
@@ -10,9 +24,9 @@ leftPwmPin = 17
 rightForwardPin = 13
 rightBackwardPin = 6
 rightPwmPin = 5
-pwmFrequency = 1000
-maxInputValue = 150
-maxDutyCycle = 30
+pwmFrequency = 2000
+maxInputValue = 175
+maxDutyCycle = 20
 steeringInputThreshold = 105
 
 GPIO.setmode(GPIO.BCM)
@@ -31,13 +45,14 @@ rightMotor = GPIO.PWM(rightPwmPin, pwmFrequency)
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+print('porterbot-controller initialization completed')
 
 @socketio.on('joystickState')
 def handle_my_custom_event(json):
     x = json['x']
     y = json['y']
     print('received: ' + str(x) + ' ' + str(y))
-
+       
     dutyCycle = (sqrt(pow(x,2) + pow(y,2)) * maxDutyCycle)  / maxInputValue
 
     if abs(x) > steeringInputThreshold: # Turn
@@ -54,7 +69,7 @@ def handle_my_custom_event(json):
         else: # Backward
             rightDutyCycle = -dutyCycle
             leftDutyCycle = -dutyCycle
-
+    
     print('leftDutyCycle: ' + str(leftDutyCycle))
     print('rightDutyCycle: ' + str(rightDutyCycle))
 
@@ -75,7 +90,7 @@ def handle_my_custom_event(json):
 
     leftMotor.ChangeDutyCycle(dutyCycle)
     rightMotor.ChangeDutyCycle(dutyCycle)
-
+    
 @socketio.on('connect')
 def test_connect():
     print('Client connected')
@@ -87,7 +102,7 @@ def test_disconnect():
     print('Client disconnected')
     leftMotor.stop()
     rightMotor.stop()
-    GPIO.cleanup()
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8080)
+
